eb2d0b2254b3d92787bb7675447fcd65fbcc93ec
Asif Nawaz <asif_nawaz96@live.com>
2020-09-17 21:43:04 +0500

    Removed Icons from Manifest.json

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header: React.FC = () => {
     return (
         <h2>
-            Expense Tracker PWA Made With TypeScript
+            Expense Tracker PWA Made With TypeScript.
         </h2>
     )
 }
